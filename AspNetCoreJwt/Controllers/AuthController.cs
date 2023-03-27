95fa27e491113aabf89fc3ab15b2e832e2dfd8c0
Sinthon <s.sinthon@cas.com.kh>
2023-03-27 23:05:50 +0700

    create a function for generate token

---
+++ AspNetCoreJwt/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AspNetCoreJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        IConfiguration _configuration { get; set; }
+
+        public AuthController(IConfiguration configuration)
+        {
+            this._configuration = configuration;   
+        }
+
+        private string CreateToken(Models.User user)
+        {
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, user.Username));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSetting::key").Value!));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
---
+++ AspNetCoreJwt/Models/DTOs/UserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace AspNetCoreJwt.Models.DTOs;
+
+public class UserDto
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
